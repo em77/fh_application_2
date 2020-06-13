0ba316de49794a480c17ab8acc2fb05d445b4499
Eric M <ericmarchese77@gmail.com>
2020-06-13 17:41:50 -0400

    Add coffeescript loader, jquery, bootstrap, moment.js, bootstrap_form gem

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,20 @@
 const { environment } = require('@rails/webpacker')
+const coffee =  require('./loaders/coffee')
+const webpack = require('webpack')
+
+environment.loaders.prepend('coffee', coffee)
+
+environment.plugins.insert(
+  'IgnorePlugin',
+  new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/)
+)
+
+environment.plugins.prepend('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery',
+    Popper: ['popper.js', 'default']
+  })
+)
 
 module.exports = environment
