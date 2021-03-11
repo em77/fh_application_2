aefc04638388c6a3d736e808e45980a636b01b69
Eric M <ericmarchese@protonmail.com>
2021-03-10 20:11:34 -0500

    Update rails, remove unused files

--- spec/mailers/previews/member_application_mailer_preview.rb
+++
@@ -1,4 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/member_application_mailer
-class MemberApplicationMailerPreview < ActionMailer::Preview
-
-end
--- spec/requests/member_applications_request_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe "MemberApplications", type: :request do
-
-end
