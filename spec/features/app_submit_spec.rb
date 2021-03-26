3bdf7ae8bb963660509c0694bc2088d55491d573
Eric M <ericmarchese@protonmail.com>
2021-03-26 16:23:27 -0400

    Add test for MemberApplication creation with missing required attributes

--- spec/features/app_submit_spec.rb
+++ spec/features/app_submit_spec.rb
@@ -65,6 +65,40 @@ feature "app submit" do
     expect(page).to_not have_content("Application Submitted!")
   end
 
+  scenario "user cannot submit an application with missing required attributes", js: true do
+    visit new_member_application_path
+    page.attach_file(
+      "member_application_psych_eval",
+      Rails.root.join("lib/sample_attachments/notes.pdf"),
+      make_visible: true
+    )
+    page.attach_file(
+      "member_application_psych_social",
+      Rails.root.join("lib/sample_attachments/utopia.pdf"),
+      make_visible: true
+    )
+    page.attach_file(
+      "member_application_insurance_card",
+      Rails.root.join("lib/sample_attachments/sun.jpg"),
+      make_visible: true
+    )
+    fill_out_app(true)
+
+    # Remove phone numbers
+    fill_in :member_application_phone_number_landline, with: ''
+    fill_in :member_application_phone_number_cell, with: ''
+
+    accept_confirm do
+      click_on "Submit Application"
+    end
+
+    sleep 10
+
+    expect(page).to_not have_content "Your application was submitted successfully"
+    expect(page).to_not have_content("Application Submitted!")
+    expect(ActionMailer::Base.deliveries).to be_empty
+  end
+
   scenario "user saves draft, then adds the missing attachment, then submits without phone number and then successfully submits", js: true do
     visit new_member_application_path
     page.attach_file(
@@ -107,6 +141,7 @@ feature "app submit" do
     expect(ActionMailer::Base.deliveries).to be_empty
     expect(page).to_not have_content("Application Submitted!")
 
+    # Restore a phone number
     fill_in :member_application_phone_number_cell, with: Faker::Number.number(digits: 10)
 
     expect_any_instance_of(Twilio::REST::Client).to receive_message_chain('lookups.phone_numbers.fetch').and_return({ 'type' => 'mobile' })
